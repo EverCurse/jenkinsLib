39cbfa4d72908ba8822d7d174d535669b98e1131
孙麒麟 <sunqilin@haixue.com>
2020-06-08 13:55:50 +0800

    add build func

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,9 +1,16 @@
 package org.devops
 
 //打包流程
-def Build(buildType, buildShell){
+def Build(buildType){
   def buildTools = ["mvn":"m3", "npm":"npm"]
   println("当前打包的类型为 ${buildType}")
+  if (buildType == "mvn"){
+    def buildShell = "-v"
+  }else if (buildType == "npm"){
+    def buildShell = "-v"
+  }else {
+    def buildShell = "-unknown"
+  }
   buildHome = tool buildTools[buildType]
   sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
