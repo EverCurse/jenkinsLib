c00f366d0aa46ea4179de90baf283c937b4e5e14
EverCurse <591356683@qq.com>
2020-06-08 15:06:43 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,12 +1,12 @@
 package org.devops
 
 //打包流程
-def Build(buildType){
+def Build(buildType, buildShell){
   def buildTools = ["mvn":"m3", "npm":"npm"]
   println("当前打包的类型为 ${buildType}")
   String buildShell = ""
   if (buildType == "mvn"){
-    buildShell = "-v"
+    println("mvn install package...")
   }else if (buildType == "npm"){
     buildShell = "-v"
   }else {
